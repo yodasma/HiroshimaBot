d91b601cf0bf726af20d3c3f6e0c4a81d949a95e
yodasma <yodas.email13@gmail.com>
2024-05-26 21:17:27 +0200

    Update botinfo.js

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -18,14 +18,14 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
 
             embed.setTitle(`Information à Propos De : ${client.user.username}`)
-            embed.setURL('https://discord.gg/9ZfB8m5E88')
+            embed.setURL('https://discord.gg/BHPnwRYcg3')
             embed.setDescription('**Une description complète et détaillée du Bot** <@1225087364821684379>')
             embed.setColor(color)
             .setThumbnail(message.author.avatarURL({ dynamic:true }));
             embed.setTimestamp()
             embed.setFooter(`${client.config.name}`)
             embed.addFields(
-                { name: '👑 Owner / Developer :', value: '<@1208337813339373569>', inline: true },
+                { name: '👑 Owner / Developer :', value: '<@1164678741717430313>', inline: true },
                 { name: '🔌 Latence Ping Bot :', value: `\`${client.ws.ping}Ms\`` },
                 { name: '🚀 Total Server(s) :', value: `\`${client.guilds.cache.size}\``, inline: true },
                 { name: '👥 Total User(s) :', value: `\`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\``, inline: true },
